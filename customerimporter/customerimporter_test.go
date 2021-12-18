a7606714be8bc275bf18afd72151039693637503
Daniel O'Connor <daniel.o.c.03@gmail.com>
2021-12-18 18:31:59 +0000

    Add one test

--- customerimporter/customerimporter_test.go
+++ customerimporter/customerimporter_test.go
@@ -1,1 +1,29 @@
 package customerimporter
+
+import (
+	"reflect"
+	"testing"
+)
+
+type TestCases struct {
+	inputFile string
+	expected  map[string]int
+}
+
+var testCases = []TestCases{
+	{inputFile: "testdata/test-1.csv", expected: map[string]int{"github.io": 1}},
+}
+
+func TestReadFile(t *testing.T) {
+	for _, test := range testCases {
+		result, err := ImportCustomers(test.inputFile)
+		if err != nil {
+			t.Fatal("Expected Result not given")
+		}
+
+		if !reflect.DeepEqual(result, test.expected) {
+			t.Fatalf("Expected Domains \n%v\n did not equal actual Domains \n%v\n", result, test.expected)
+		}
+
+	}
+}
