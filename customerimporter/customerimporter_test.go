f653507b92ef94edb73aa2b9825d8fc2cbf36243
Daniel O'Connor <daniel.o.c.03@gmail.com>
2021-12-18 19:14:51 +0000

    Expect error for file that does not exist, expect graceful handling of users without domains

--- customerimporter/customerimporter_test.go
+++ customerimporter/customerimporter_test.go
@@ -6,22 +6,28 @@ import (
 )
 
 type TestCases struct {
-	inputFile string
-	expected  map[string]int
+	inputFile     string
+	expected      map[string]int
+	expectedError bool
 }
 
 var testCases = []TestCases{
 	{inputFile: "testdata/single-customer.csv", expected: map[string]int{"github.io": 1}},
 	{inputFile: "testdata/multiple-customers.csv", expected: map[string]int{"github.io": 1, "cyberchimps.com": 1, "hubpages.com": 1}},
 	{inputFile: "testdata/multiple-customers-single-domain.csv", expected: map[string]int{"github.io": 3}},
-	{inputFile: "testdata/multiple-customers-single-domain.csv", expected: map[string]int{"github.io": 3}},
+
+	// Unhappy Path
+	{inputFile: "testdata/this-file-does-not-exist.csv", expectedError: true},
 }
 
 func TestReadFile(t *testing.T) {
 	for _, test := range testCases {
 		result, err := ImportCustomers(test.inputFile)
+		if test.expectedError && err == nil {
+			t.Fatal("Expected Error to be returned")
+		}
 		if err != nil {
-			t.Fatal("Expected Result not given")
+			t.Fatalf("Did not expect to receive error: \n%v\n", err)
 		}
 
 		if !reflect.DeepEqual(result, test.expected) {
